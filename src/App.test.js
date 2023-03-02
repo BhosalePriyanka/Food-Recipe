32421b4c933405fcf546f5e5a9c9c3fc96c3b8b8
BhosalePriyanka <priyanka.patil970@gmail.com>
2023-03-02 15:32:06 +0000

    new changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Recipe from './Recipe';
 
-test('renders learn react link', () => {
+test('render submit button on screen', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByRole("button");
   expect(linkElement).toBeInTheDocument();
 });
+
+test("render search box", () =>{
+  render(<App />);
+  const inputElement = screen.getByPlaceholderText("Enter recipe name")
+  expect(inputElement).toBeInTheDocument();
+})
+
+test("render recipe child component", async() =>{
+  render(<App/>)
+  const recipeComp = await screen.findByText("Basic Homemade Harissa Recipe")
+  expect(recipeComp).toBeInTheDocument();
+})
